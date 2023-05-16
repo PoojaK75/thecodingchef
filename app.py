18f67480b50a0b537279e6febc32748f870993d2
Pooja <poojapotala@gmail.com>
2023-05-16 22:41:36 +0530

    second commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, url_for
 from views import views
 
 app = Flask(__name__)
-app.register_blueprint(views, url_prefix="/views")
+app.register_blueprint(views, url_prefix="/")
 
 
 if __name__ == '__main__':
